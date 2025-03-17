0410bb7dbbab290390415aa2351e0e42fb2c4216
Askar <a.khaimuldin@pin-up.tech>
2025-03-17 09:20:39 +0500

    added multiply function

--- src/Main.java
+++ src/Main.java
@@ -25,12 +25,37 @@ public class Main {
         return x;
     }
 
+    public static int multiply(int a, int b) {
+        int s = 0;
+
+        for (int i = 0; i < b; i++) {
+            s += a;
+        }
+
+        return s;
+    }
+
+    public static int multiplyRecursive(int a, int b) {
+        return multiplyRecursive(a, b, 0);
+    }
+
+    private static int multiplyRecursive(int a, int b, int k) {
+        if (k >= b) {
+            return 0;
+        }
+
+        return a + multiplyRecursive(a, b, k + 1);
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();
+        int y = sc.nextInt();
 
-        System.out.println("fibonacci for n=" + x + " is " + fib(x));
+        int res = multiplyRecursive(x, y);
+
+        System.out.println(res);
     }
 }
